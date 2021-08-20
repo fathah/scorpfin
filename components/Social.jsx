1399e5161f0386192d29e5e3cb96e6f779e764ad
Fathah Crg <48355244+fathah@users.noreply.github.com>
2021-08-20 22:26:41 +0530

    added fb n youtube

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,5 +1,5 @@
-import {FaReddit, FaTelegram, FaDiscord, FaTwitter} from 'react-icons/fa'
-import {AiFillTwitterCircle, AiFillRedditCircle, AiFillInstagram} from 'react-icons/ai'
+import {FaReddit, FaTelegram, FaDiscord, FaFacebook} from 'react-icons/fa'
+import {AiFillTwitterCircle, AiFillRedditCircle, AiFillInstagram, AiFillYoutube, AiFillFacebookCircle} from 'react-icons/ai'
 
 const Social = () => {
     const common = "text-xl mx-2";
@@ -8,13 +8,14 @@ const Social = () => {
         style={{
             background:'rgba(31, 35, 43, 0.7)'
         }}
-        className="p-2 flex justify-end  xl:px-48 lg:px-32"
+        className="flex justify-end  xl:px-48 lg:px-32  px-10 py-4"
     >
        <a href=""> <FaTelegram className={common}/></a>
        <a href=""> <AiFillTwitterCircle className={common}/></a>
        <a href=""> <FaDiscord className={common}/></a>
        <a href=""> <AiFillRedditCircle className={common}/></a>
        <a href=""> <AiFillInstagram className={common}/></a>
+       <a href=""> <FaFacebook className={common}/></a> 
 
     </div>);
 }
