720f3551a755e8111810e1d95833642dd411325f
Fathah Crg <48355244+fathah@users.noreply.github.com>
2021-08-20 22:30:32 +0530

    dup prop fixed

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,14 +1,13 @@
-import {FaReddit, FaTelegram, FaDiscord, FaFacebook} from 'react-icons/fa'
-import {AiFillTwitterCircle, AiFillRedditCircle, AiFillInstagram, AiFillYoutube, AiFillFacebookCircle} from 'react-icons/ai'
+import { FaTelegram, FaDiscord, FaFacebook} from 'react-icons/fa'
+import {AiFillTwitterCircle, AiFillRedditCircle, AiFillInstagram, } from 'react-icons/ai'
 
 const Social = () => {
     const common = "text-xl mx-2";
     return (<div
-    className="py-4"
+    className="py-4 flex justify-end  xl:px-48 lg:px-32  px-10 py-4"
         style={{
             background:'rgba(31, 35, 43, 0.7)'
         }}
-        className="flex justify-end  xl:px-48 lg:px-32  px-10 py-4"
     >
        <a href=""> <FaTelegram className={common}/></a>
        <a href=""> <AiFillTwitterCircle className={common}/></a>
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ export default function Index() {
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       </Head>
      <Particle/>
-    
+
       <NavBar/>
       <header>
 <div
