de10a86ca6267b5990605e0eb260ec6e4d448a64
Fathah Crg <48355244+fathah@users.noreply.github.com>
2021-08-31 18:38:39 +0530

    tokenomicsIntitated

--- components/Home/Tokenomics.jsx
+++ components/Home/Tokenomics.jsx
@@ -1,23 +1,57 @@
+import Lottie from "lottie-react";
+import tokenomics from "@/assets/tokenomics.json";
+import Tilt from 'react-parallax-tilt';
+
 const Tokenomics = () => {
+
+
+
     return (
 
         <div className="lg:px-32 xl:px-48 py-10 p-10">
- <h1 className="gr-text font-bold text-xl lg:text-2xl mb-4" data-aos="zoom-in">
-                  TOKENOMICS</h1>
-<p className="text-justify"  data-aos="fade-left">
-There is a maximum supply of 1 Quadrillion SCORPFIN tokens. 2% of these tokens have 
-been allocated to incentivize usage of the merchant network. 
-Several rewards initiatives will be launched that will compensate users of the Scorpion merchant 
-network with SCORPFIN tokens.
-<br />
-  The main reason we created the ScorpFin token is for fundraising our project. 
-One of the primary benefits of holding our SCORPFIN token is that you will be able 
-to use it in the ScorpFin app for everyday Purchases.
-<br />
-The Scorpion Finance token (SCORPFIN) will play a critical role in how the ScorPay application functions.
-<br />
-If you share our dream and support our concept, onboard to our SCORPFIN
+
+
+
+                  <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-start">
+   
+    <div className="lg:w-6/12">
+    <Tilt
+    glareEnable={true} glareMaxOpacity={0.8} glareColor="#ffffff" 
+    glarePosition="bottom" glareBorderRadius="20px"
+    ><div
+    className="main-features-grid flex flex-col items-center p-6 rounded-lg m-4"
+    style={{
+        minHeight:"350px"
+    }}
+    >
+       
+
+<h1 className="gr-text font-bold text-xl lg:text-2xl my-4" 
+data-aos="zoom-in">
+TOKENOMICS</h1>
+<p
+className="text-justify lg:text-xl"
+>
+<span className="gr-text">100 Billion</span>  Total Supply <br />
+<span className="gr-text">63%</span> Token Sales  <br />
+<span className="gr-text">10%</span> Ecosystem and community <br />
+<span className="gr-text">8%</span>    Development, marketing and R&D) <br />
+<span className="gr-text"> 4%</span>    Founders and Team <br />
+<span className="gr-text">15%</span> Farming and staking <br />
+
+
+  
 </p>
+    </div> </Tilt>
+    
+    
+    </div>
+
+    <div className="lg:w-6/12 hidden lg:flex justify-end ">
+     <Lottie animationData={tokenomics} className="w-10/12"  />
+    </div>
+    </div>
+
 
         </div>
     );
